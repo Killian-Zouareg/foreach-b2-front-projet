b74a7f7286e9fd526694b13e7945610db1e22586
killian.zouareg <killian.zouareg@vadesecure.com>
2022-02-18 09:32:32 +0100

    added button component

---
+++ projet-sacha/src/components/button.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <v-button @click="checkresult">
+            VALIDATE
+        </v-button>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'button',
+    methods: {
+         async checkResult(){
+        console.log("TEST BOUTON");
+        if (this.albumName.replace(/ /g,"").toLowerCase() === this.song.albumName || this.artistName.replace(/ /g,"").toLowerCase() == this.song.artistName){
+            console.log('Gagné');
+            this.incrementActual()
+            this.incrementMax()
+            this.albumName = ""
+            this.artistName =""
+            this.generateCover()
+            this.success = true
+            setTimeout(() => {
+              this.success = false
+            }, 1500)
+        }else {
+            this.fail = true
+            setTimeout(() => {
+              this.fail = false
+            }, 1500)
+            this.incrementMax()
+            this.albumName = ""
+            this.artistName = ""
+            this.generateCover()
+        }
+      }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
